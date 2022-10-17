fa2654884db748e1fa4b01b441b6e092436cd167
SA-locator <115512617+SA-locator@users.noreply.github.com>
2022-10-18 07:51:33 +1300

    Add files via upload

---
+++ solid-principles-example-main/solid-principles-example-main/src/main/java/com/solid/isp/Paytm.java
@@ -0,0 +1,14 @@
+package com.solid.isp;
+
+public class Paytm implements UPIPayments {
+
+    public void payMoney() {
+
+    }
+
+    public void getScratchCard() {
+
+    }
+
+
+}
---
+++ solid-principles-example-main/solid-principles-example-main/src/main/java/com/solid/lsp/solution/SocialVideoCallManager.java
@@ -0,0 +1,5 @@
+package com.solid.lsp.solution;
+
+public interface SocialVideoCallManager {
+    public void groupVideoCall(String... users);
+}
